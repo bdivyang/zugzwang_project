39ae3a6e64f9b184d6b3b757fec3bd84007a3594
bdivyang <bharath.divyang@gmail.com>
2014-04-24 17:42:36 +0530

    Initialize Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZugzwangProject::Application.config.secret_key_base = '5ccfbcc1e853a88918994a0f0bfa733ce51da9d8a3454de10d48f560071b74933368f25f0ec7481e765455e1123c472078634b0c6865d66e02d9d9379244de73'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ZugzwangProject::Application.config.session_store :cookie_store, key: '_zugzwang_project_session'
